9c83f5b95bae3073778c1a1011a4e1955af6dd66
Ricardo Gamboa <rgamboaa@wearegap.com>
2024-10-17 09:24:18 -0600

    initial commit

---
+++ Admin-Console-Health-Check-Service/Admin-Console-Health-Check-Service/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Admin_Console_Health_Check_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+    }
+}
